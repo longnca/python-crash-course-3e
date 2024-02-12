575c2e93ea5de82f2deb487f33c6ce892d6597c2
longbytes <dsbuff01@gmail.com>
2024-02-11 20:34:01 -0700

    update github api

--- Chapter_17/python_repos.py
+++ Chapter_17/python_repos.py
@@ -2,9 +2,9 @@ import requests
 
 # Make an API call and check the response
 url = "https://api.github.com/search/repositories"
-url += "?q=language:python+sort:stars+stars:>1000"
+url += "?q=language:python+sort:stars+stars:>10000"
 
-headers = {"Accept": "application.vnd.github.v3+json"}
+headers = {"Accept": "application/vnd.github.v3+json"}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 
@@ -13,3 +13,19 @@ response_dict = r.json()
 
 # Process results
 print(response_dict.keys())
+
+# Convert the response object to a dictionary
+response_dict = r.json()
+
+print(f"Total repositories: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
+
+# Explore information about the repositories
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# Examine the first repository
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
