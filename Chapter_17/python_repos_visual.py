b126e8097105bd78efee11272496f565af070822
longbytes <dsbuff01@gmail.com>
2024-02-14 18:26:27 -0700

    finish python_repos_visual.py

--- Chapter_17/python_repos_visual.py
+++ Chapter_17/python_repos_visual.py
@@ -20,11 +20,28 @@ print(f"Complete results: {not response_dict['incomplete_results']}")
 
 # Explore information about the repositories
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_links, stars, hover_texts = [], [], []
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+
     stars.append(repo_dict['stargazers_count'])
 
+    # Build hover texts.
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    hover_text = f"{owner}<br />{description}"
+    hover_texts.append(hover_text)
+
 # Make visualization
-fig = px.bar(x=repo_names, y=stars)
+title = "Most-Starred Python Projects on GitHub"
+labels = {'x': 'Repository', 'y': 'Stars'}
+fig = px.bar(x=repo_links, y=stars, title = title, labels=labels,
+             hover_name=hover_texts)
+
+fig.update_layout(title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20)
+fig.update_traces(marker_color='SteelBlue', marker_opacity=0.6)
+
 fig.show()
