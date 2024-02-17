2f555c620dfc6bcceb1dfaa2a294f2457d805cf7
longbytes <dsbuff01@gmail.com>
2024-02-17 09:09:36 -0700

    add exercise 17-1

--- Chapter_17/17-1-other_languages.py
+++ Chapter_17/17-1-other_languages.py
@@ -1,8 +1,9 @@
 import requests
+import plotly.express as px
 
 # Make an API call and check the response
 url = "https://api.github.com/search/repositories"
-url += "?q=language:python+sort:stars+stars:>10000"
+url += "?q=language:javascript+sort:stars+stars:>10000"
 
 headers = {"Accept": "application/vnd.github.v3+json"}
 r = requests.get(url, headers=headers)
@@ -19,27 +20,28 @@ print(f"Complete results: {not response_dict['incomplete_results']}")
 
 # Explore information about the repositories
 repo_dicts = response_dict['items']
-print(f"Repositories returned: {len(repo_dicts)}")
-
-# Examine the first repository
-repo_dict = repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
-#for key in sorted(repo_dict.keys()):
-#    print(key)
-
-print("\nSelected information about first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
-
-print("\nSelected information about each repository:")
+repo_links, stars, hover_texts = [], [], []
 for repo_dict in repo_dicts:
-    print(f"Name: {repo_dict['name']}")
-    print(f"Owner: {repo_dict['owner']['login']}")
-    print(f"Stars: {repo_dict['stargazers_count']}")
-    print(f"Repository: {repo_dict['html_url']}")
-    print(f"Description: {repo_dict['description']}")
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+
+    stars.append(repo_dict['stargazers_count'])
+
+    # Build hover texts.
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    hover_text = f"{owner}<br />{description}"
+    hover_texts.append(hover_text)
+
+# Make visualization
+title = "Most-Starred Python Projects on GitHub"
+labels = {'x': 'Repository', 'y': 'Stars'}
+fig = px.bar(x=repo_links, y=stars, title = title, labels=labels,
+             hover_name=hover_texts)
+
+fig.update_layout(title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20)
+fig.update_traces(marker_color='SteelBlue', marker_opacity=0.6)
+
+fig.show()
