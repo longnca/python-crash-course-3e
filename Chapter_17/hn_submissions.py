0c69b81a7fdf8c10eb91b8830b8a52c2432a21e2
longbytes <dsbuff01@gmail.com>
2024-02-17 09:00:48 -0700

    finish fetching data from hackernews

--- Chapter_17/hn_submissions.py
+++ Chapter_17/hn_submissions.py
@@ -7,7 +7,7 @@ url = "https://hacker-news.firebaseio.com/v0/topstories.json"
 r = requests.get(url)
 print(r.status_code)
 
-# Process info about each submission
+# Convert to a Python list
 submission_ids = r.json()
 
 submission_dicts = []
@@ -25,3 +25,12 @@ for submission_id in submission_ids[:5]:
         'comments': response_dict['descendants'],
     }
     submission_dicts.append(submission_dict)
+
+# Sort the dictionary based on comments    
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
+
+# Print the dictionary 
+for submission_dict in submission_dicts:
+    print(f"\nTitle: {submission_dict['title']}")
+    print(f"Discussion link: {submission_dict['hn_link']}")
+    print(f"Comments: {submission_dict['comments']}")
