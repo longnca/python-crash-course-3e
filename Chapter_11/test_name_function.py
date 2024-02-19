e77d330bfc89558f9a9f89735c7fe8b9b3e3677a
longbytes <dsbuff01@gmail.com>
2024-02-18 17:21:02 -0700

    add pytest for name_function.py

--- Chapter_11/test_name_function.py
+++ Chapter_11/test_name_function.py
@@ -1,1 +1,11 @@
-from name_function import 
+from name_function import get_formatted_name
+
+def test_first_last_name():
+    """Do names like 'Bradley M. Cooper' work?"""
+    formatted_name = get_formatted_name('bradley', 'cooper')
+    assert formatted_name == 'Bradley Cooper'
+
+def test_first_last_middle_name():
+    """Do names like 'John F. Kennedy' work?"""
+    formatted_name = get_formatted_name('john', 'kennedy', 'f.')
+    assert formatted_name == 'John F. Kennedy'
