3401d4b2cc51a087280c59ddedd83933e3c57c2a
longbytes <dsbuff01@gmail.com>
2024-02-20 16:16:45 -0700

    add pytest

--- Chapter_11/language_survey.py
+++ Chapter_11/language_survey.py
@@ -1,3 +1,18 @@
 from survey import AnonymousSurvey
 
 # Define a question and make a survey
+question = "What language did you first learn to speak?"
+language_survey = AnonymousSurvey(question)
+
+# Show the question and store the responses to the question
+language_survey.show_question()
+print(f"Enter 'q' at any time to quit.\n")
+while True:
+    response = input("Language: ")
+    if response == 'q':
+        break
+    language_survey.store_response(response)
+
+# show the survey results
+print("\nThank you to everyone who participate in the survey.")
+language_survey.show_results()
