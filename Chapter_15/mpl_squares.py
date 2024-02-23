8beaea703f462551457c6ac37aaefe1d980079e9
longbytes <dsbuff01@gmail.com>
2024-02-23 10:48:47 -0700

    add plots in Chap 15

--- Chapter_15/mpl_squares.py
+++ Chapter_15/mpl_squares.py
@@ -12,7 +12,7 @@ ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
-# Set size of tick labels.
+# Set size of tick labels
 ax.tick_params(labelsize=14)
 
 plt.show()
