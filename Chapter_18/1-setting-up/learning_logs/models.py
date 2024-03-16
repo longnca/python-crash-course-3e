7be5545423b72f03bb65718f86648d61e9f5be87
longbytes <dsbuff01@gmail.com>
2024-03-16 12:28:28 -0600

    add exercise 18-2 to Chapter 18: add an if statement to the __str__() method

--- Chapter_18/1-setting-up/learning_logs/models.py
+++ Chapter_18/1-setting-up/learning_logs/models.py
@@ -19,4 +19,7 @@ class Entry(models.Model):
 
     def __str__(self) -> str:
         """Return a simple string representing the entry."""
-        return f"{self.text[:50]}..."
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return f"{self.text}"
