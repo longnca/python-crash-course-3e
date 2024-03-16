1b4bf5b83a2d79786d3395d1ab5263ba1c00e928
longbytes <dsbuff01@gmail.com>
2024-03-16 17:48:18 -0600

    complete exercise 18-5 of a meal planner app

--- Chapter_18/18-5-meal-planner/meal_plans/urls.py
+++ Chapter_18/18-5-meal-planner/meal_plans/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ll_project project.
+URL configuration for meal_plans project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("learning_logs.urls")),
+    path("", include("meal_plans_app.urls")),
 ]
