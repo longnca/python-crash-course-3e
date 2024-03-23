6c6b85386787ab197a80b67ea0bd6df38019798a
longbytes <dsbuff01@gmail.com>
2024-03-23 17:31:57 -0600

    add new_topic() function and url

--- Chapter_18/learning_logs_app_project/learning_logs/urls.py
+++ Chapter_18/learning_logs_app_project/learning_logs/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path("topics/", views.topics, name="topics"),
     # detail page for a single topic
     path("topics/<int:topic_id>/", views.topic, name='topic'),
+    # page for adding a new topic
+    path("new_topic/", views.new_topic, name="new_topic"),
 ]
